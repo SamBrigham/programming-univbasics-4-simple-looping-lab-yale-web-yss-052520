5459b63662408c84f91c4e31c8265b5d1d365541
Samuel Brigha <sam.brigham@yale.edu>
2020-05-12 18:31:12 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -8,12 +8,12 @@ def loop_message_five_times(string)
 end
 end
 
-def loop_message_five_times(string2, integer)
-  counter2 = 0
-  while string2
-  puts string2
-  counter2 += 1
-  break if counter2 > integer
+def loop_message_n_times(string2, integer)
+  counter = 0
+  while string
+  puts string
+  counter += 1
+  break if counter > integer
 end
 end
 p loop_message_five_times("bob")
